2ad0f9dd454c5acb68a9a85365ea66ff63d83d81
Stephen <sfoster2010.sf@gmail.com>
2023-03-02 09:22:41 -0500

    getting js setup

--- js/site.js
+++ js/site.js
@@ -1,4 +1,17 @@
-// Call Hello World function
-function helloWorld() {
-    alert("Hello World");
+//Get the values from the page
+//Start or controller function
+function getValues() {
+
+}
+
+//Generate numbers from the start value to the end value
+//Logic function(s)
+function generateNumbers() {
+
+}
+
+//Display the numbers and mark the even ones bold
+//Display or view functions
+function displayNumbers() {
+
 }
